9fd1356d2d9a09d3a6d3f6df6dde26fc817b98b4
federico-maioli <fed.maioli@gmail.com>
2025-07-17 16:11:59 +0200

    1 st update script and gitignore

---
+++ R/01_clean_medits.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+library(tidylog) # gives info on data wrangling
+library(here)
+library(janitor)
+library(readxl)
+library(rfishbase)
+library(geosphere) # for mean trawl position
+library(rnaturalearth)
+library(rnaturalearthdata)
+library(sf)
